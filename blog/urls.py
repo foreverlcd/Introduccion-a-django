abb5975541b0498407dd1bf217cbcdc42bdc2de0
foreverlcd <jhonjesuquispemachaca@gmail.com>
2024-12-25 12:22:14 -0500

    Adding detail for posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import BlogListView, BlogCreateView
+from .views import BlogListView, BlogCreateView, BLogDetailView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
     path('create/', BlogCreateView.as_view(), name='create'),
+    # Tenemos que pasarle un id para que sepa que post mostrar
+    path('<int:pk>/', BLogDetailView.as_view(), name='detail'),
 ]
